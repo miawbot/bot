c530d1cb8074591748d79d201dcabee4358e5ce7
miawbot <107809540+miawbot@users.noreply.github.com>
2023-01-27 14:39:25 +0700

    Update edu.sh

--- websocket/edu.sh
+++ websocket/edu.sh
@@ -26,7 +26,7 @@ exit 0
 fi
 clear
 # Link Hosting Kalian
-akbarvpn="raw.githubusercontent.com/lizsvr/cfnfree/main/websocket"
+akbarvpn="raw.githubusercontent.com/miawbot/bot/main/websocket"
 
 # Getting Proxy Template
 wget -q -O /usr/local/bin/ws-nontls https://${akbarvpn}/websocket.py
